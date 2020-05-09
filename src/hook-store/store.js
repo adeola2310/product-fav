249571350e69811f32eee7472b540ee7e081c87d
Adeola Adekoyejo <adekoyejoadeola@gmail.com>
2020-05-09 17:16:27 +0100

    getting statred wt custom hooks and finishing store hook

---
+++ src/hook-store/store.js
@@ -0,0 +1,42 @@
+import React, {useEffect, useState} from "react";
+
+// managing our global state
+let globalState = {}
+
+let listener = [];
+
+let actions = {}
+
+
+// lets create a custom hook
+export const useStore = ()=>{
+   const setState =  useState(globalState)[1];
+
+   const dispatch = actionIdentifier => {
+       const newState = actions[actionIdentifier](globalState);
+       globalState = {...globalState, ...newState};
+
+
+       for(const listen of listener){
+           listen(globalState)
+       }
+   }
+
+   useEffect( ()=>{
+       listener.push(setState);
+
+       return ()=>{
+           listener = listener.filter(li => li !== setState);
+       }
+   }, [setState]);
+
+   return [globalState, dispatch];
+}
+
+export const initStore = (userActions, initialState) => {
+    if (initialState){
+        globalState = {...globalState, ...initialState};
+    }
+
+    actions = { ...actions, ...userActions};
+}
